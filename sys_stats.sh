b42d8b95a8529c380f5b0dbcbf0b91f4e9dbd8d9
samroedl <115032842+samroedl@users.noreply.github.com>
2022-10-11 12:24:56 -0600

    Update sys_stats.sh

--- sys_stats.sh
+++ sys_stats.sh
@@ -1,41 +1,38 @@
-# add shebang here
-# add any contributor names in a comment here
-
+#!/bin/bash
+#Editor: Sam Roedl
 #************************************#
 #******* OUTPUT SYSTEM STATS ********#
 #************************************#
 
 # Make a directory called System_Stats and change directories to that directory
-# Try to make the output for each file look as neat and organized as you can.
+# Try to make the output for each file look as neat and organized as y
+mkdir System_Stats
+chmod o+rwx System_Stats
+cd System_Stats
 
 # Output the following information to a file called kernel
-    # 1. Kernel Name
-    # 2. Kernel Release
-    # 3. Kernel Version
-    # 4. The operating system
+     uname -s > Kernel
+  uname -r >> Kernel
+   uname -v >> Kernel
+   uname -o >> Kernel
 
 
 # Output the following information to a file called network
-    # 1. Do not print any serial numbers or sensitive information related to the system
-    # 2. All network interfaces
-    
-    
+
+    lshw -short -sanitize > network
+
+
 # Output the following information to a file called disk.html
-    # 1. All disks
-    # 2. The output should include html tags, i.e. <html></html>
+
+   lshw -html > disk.html
 
 
 # Output the following information to a file called cpu
-    # 1. The first five lines of the command lscpu
-    # 2. The last 12 lines of the command lscpu
-    
+
+   lscpu | head -n5 > cpu
+   lscpu | tail -n16 >> cpu
+
 
 # Output the following information to a file called block_dev
-    # 1. Only the name, size, and type of the block devices
-    # 2. The output should use ascii characters for any tree formatting
-    
-    
-# Output the following information to a file called sata
-    # 1. Any Sata devices connected to the machine along with human readable sizes of the devices
-    # 2. More specific information about each device that is connected to the machine
-    
+
+  lsblk -i -o NAME,SIZE,TYPE > block_dev
